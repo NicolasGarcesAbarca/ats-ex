f161c996efa0a3202d543130928493efbf2d726b
Nicolas <nickogarces@gmail.com>
2022-11-22 22:24:44 -0300

    create route ats

---
+++ src/routes/ats/index.tsx
@@ -0,0 +1,15 @@
+import { component$ } from '@builder.io/qwik';
+import { DocumentHead } from '@builder.io/qwik-city';
+
+export default component$(() => {
+  
+  return (
+    <>
+     <div>hi</div> 
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: 'Ats',
+};
